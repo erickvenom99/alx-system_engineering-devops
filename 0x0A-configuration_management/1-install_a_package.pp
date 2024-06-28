3384563b16890236abeb13f5f4d7a1d51787728c
ericvenom99 <prevenakhtar@mail.com>
2024-06-28 22:39:40 +0200

    corrected task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
+#downlaod python package
 package { 'python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
 
-package { 'flask':
+#flask package
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
@@ -11,5 +13,5 @@ package { 'flask':
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['flask'],
+  require  => Package['Flask'],
 }
