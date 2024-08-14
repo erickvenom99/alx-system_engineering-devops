7ac81ace528bc575eb6acc551fa451e390d6b696
ericvenom99 <prevenakhtar@mail.com>
2024-08-14 06:55:59 +0200

    corrected task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,29 +2,35 @@
 """
 Recursive function that queries the Reddit API and
 returns a list containing the titles of
-all hot articles for a given subreddi
+all hot articles for a given subreddit.
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".
-    format(subreddit, after)
-    headers = {'User-Agent': 'custom'}
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Returns a list containing the titles of all hot articles
+    for a given subreddit.
+    """
+    reddit = "https://www.reddit.com/"
+    header = {'user-agent': 'my-app/0.0.1'}
+    if after is None:
+        return hot_list
+
+    url = reddit + "r/{}/hot/.json".format(subreddit)
+    params = {'limit': 100, 'after': after}
+
+    res = requests.get(url, headers=header, params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
 
     try:
-        res = requests.get(url, headers=headers, allow_redirects=False)
-        if res.status_code == 200:
-            data = res.json()
-            if "error" in data:
-                return None
-            hot_list.extend([post['data']['title'] for post in
-                             data['data']['children']])
-            if data['data']['after']:
-                return recurse(subreddit, hot_list, data['data']['after'])
-            else:
-                return hot_list
-        else:
-            return None
-    except requests.RequestException:
+        res_data = res.json()
+        after = res_data['data']['after']
+        hot_list.extend([child['data']['title']
+                         for child in res_data['data']['children']])
+        return recurse(subreddit, hot_list, after)
+    except ValueError:
         return None
