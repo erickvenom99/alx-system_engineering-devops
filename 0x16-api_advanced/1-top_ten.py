f7da8e06aa3a25678274b8468e33a5e472a8145a
ericvenom99 <prevenakhtar@mail.com>
2024-08-14 11:17:16 +0200

    corrected 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """
-Quries Reddit and prints the titles of the first 10
+Queries Reddit and prints the titles of the first 10
 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the tifles of the first 10
+    """Prints the titles of the first 10
        hot posts
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    try:
+        res = requests.get(url, headers=headers, allow_redirects=False)
+        if res.status_code != 200:
+            print("None")
+            return
+
+        data = res.json()['data']['children']
+
+        if len(data) == 0:
+            print("None")
+            return
+
+        for titles in data:
+            print(titles['data']['title'])
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+    except (KeyError, IndexError):
         print("None")
-
-    data = res.json()['data']['children']
-
-    if len(data) == 0:
-        print("None")
-        return
-
-    for titles in data:
-        print(titles['data']['title'])
