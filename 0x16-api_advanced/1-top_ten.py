831968722d7bb5e9df390c5e95443f3d3dc46c39
ericvenom99 <prevenakhtar@mail.com>
2024-08-14 05:51:46 +0200

    corrected task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
        hot posts
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
         print("None")
