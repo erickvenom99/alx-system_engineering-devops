cac19c229f0eb04ba2a1e48456531d5c485f5bff
ericvenom99 <prevenakhtar@mail.com>
2024-08-14 23:39:44 +0200

    update task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # fix typo error on phpp extention to php in wap-settings
 
 exec {'fix-wordpress':
-    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path     => '/usr/local/bin/:/bin/'
-    provider => shell,
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
