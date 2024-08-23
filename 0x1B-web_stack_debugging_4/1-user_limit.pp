33712482a86d8161e1d558841b72d1fc3f0e37ec
ericvenom99 <prevenakhtar@mail.com>
2024-08-23 06:16:45 +0200

    corrected task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,7 @@
-# Increase the hard file limit for the Holberton user
-exec { 'increase-hard-file-limit-for-holberton':
-    command => 'sed -i /^holberton\ hard/s/4/40000/ /etc/security/limits.conf',
-    path    => '/usr/local/bin:/bin',
+# Signin to holberton and reopen file
+file { 'SigninFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#erased'
 }
 
-exec { 'increase-soft':
-    command => 'sed -i /^holberton\ soft/s/5/50000/ /etc/security/limits.conf',
-    path    => '/usr/local/bin:/bin',
-}
